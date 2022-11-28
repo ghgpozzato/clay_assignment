45f8b89cbc7f976dddc7aaccf98240d46157abdf
Guilherme Pozzato <ghgpozzato@gmail.com>
2022-11-28 13:27:18 +0100

    Clean up

--- common/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gpozzato/Repos/clay_assignment/common/src
-# Build directory: /home/gpozzato/Repos/clay_assignment/common/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test/CMakeFiles/run_ut_kvp_storage.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for run_ut_kvp_storage.
-# This may be replaced when dependencies are built.
